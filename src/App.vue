85c3d13df081aa779204837350de846cbadf9db5
James Fenwick <j.fenwick@me.com>
2026-01-01 20:20:14 +0000

    Add editable name

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import {onMounted, ref, watch} from "vue";
+import EditableContent from "./components/EditableContent.vue";
+
+type State = {
+  name?: string;
+}
+
+const state = ref<State>({});
+watch(state, (value) => localStorage.setItem('state', JSON.stringify(value)), {
+  deep: true,
+});
+
+onMounted(() => {
+  const item = localStorage.getItem('state');
+  if (!item) {
+    return;
+  }
+  state.value = JSON.parse(item) as State;
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <EditableContent v-model="state.name">default</EditableContent>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/components/EditableContent.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+interface Props {
+  element?: string;
+}
+
+const model = defineModel<string>({
+  default: () => 'Character',
+});
+withDefaults(defineProps<Props>(), {
+  element: () => 'h1',
+});
+
+function onBlur(e: InputEvent) {
+  model.value = e.target?.innerText;
+}
+</script>
+
+<template>
+  <component
+      :is="element"
+      contenteditable="plaintext-only"
+      @blur="onBlur"
+  >
+    {{ model }}
+  </component>
+</template>
