3c248849e2597893233e347e8e174e5ce65c121e
James Fenwick <j.fenwick@me.com>
2026-01-05 18:59:48 +0000

    Convert to PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,39 @@
+import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), VitePWA({
+    strategies: 'injectManifest',
+    srcDir: 'src',
+    filename: 'sw.ts',
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'Rivers of London Character Tracker',
+      short_name: 'RoL: Tracker',
+      description: 'Rivers of London Character Tracker',
+      theme_color: '#ffffff',
+    },
+
+    injectManifest: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
   server: {
     port: 7650,
   },
