2572f77a3af824807267571f66f0126594fd825e
James Fenwick <j.fenwick@me.com>
2026-05-10 20:27:40 +0100

    Add impaired switch

--- src/components/Character.vue
+++ src/components/Character.vue
@@ -23,6 +23,15 @@ defineProps<{character: Character}>();
         <div class="fs-1 text-center fw-semibold">{{ character.luck }}</div>
       </div>
     </div>
+
+    <div class="card border-secondary">
+      <label class="card-header small bg-transparent" for="impaired">Impaired</label>
+      <div class="card-body">
+        <div class="form-check form-switch form-switch-md">
+          <input class="form-check-input" type="checkbox" id="impaired" switch>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -33,10 +42,21 @@ defineProps<{character: Character}>();
 .character {
   display: grid;
   grid-template-areas: 'header header' 'magic luck';
+  grid-template-columns: repeat(2, 1fr);
   gap: map-get($spacers, 3);
 }
 
 .character h1 {
   grid-area: header;
+}
+
+.form-switch.form-switch-md {
+  margin-bottom: 1rem; /* JUST FOR STYLING PURPOSE */
+}
+
+.form-switch.form-switch-md .form-check-input {
+  height: 1.5rem;
+  width: calc(2rem + 0.75rem);
+  border-radius: 3rem;
 }
 </style>
